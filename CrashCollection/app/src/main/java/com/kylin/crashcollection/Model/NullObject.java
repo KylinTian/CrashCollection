8f13c35341e398ca5c08c9ff10e914336c205a2b
tianzhengzheng <tianzhengzheng@ivtime.com>
2018-01-05 17:09:01 +0800

    [cf]项目框架

---
+++ CrashCollection/app/src/main/java/com/kylin/crashcollection/Model/NullObject.java
@@ -0,0 +1,9 @@
+package com.kylin.crashcollection.Model;
+
+/**
+ * Created by Einstein on 2018/1/5.
+ * NullObject for NullPointerException.
+ */
+public class NullObject extends Object {
+    public long id;
+}
--- CrashCollection/app/src/main/java/com/kylin/crashcollection/activities/MainActivity.java
+++ CrashCollection/app/src/main/java/com/kylin/crashcollection/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.kylin.crashcollection.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.kylin.crashcollection.Model.NullObject;
 import com.kylin.crashcollection.R;
 import com.kylin.crashcollection.activities.base.BaseActivity;
 
@@ -11,6 +14,15 @@ import com.kylin.crashcollection.activities.base.BaseActivity;
  * CrashCollection MainActivity
  */
 public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+
+    private TextView tv_click_crash;
+
+    /**
+     * NullObject for NullPointerException.
+     */
+    private NullObject mNullObject = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,16 +30,34 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         initViews();
     }
 
+    /**
+     * Init Views.
+     */
     private void initViews() {
-
+        tv_click_crash = (TextView) findViewById(R.id.tv_click_crash);
+        tv_click_crash.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.tv_click_crash:
+                click2Crash();
+                break;
             default:
                 break;
         }
+    }
+
+    /**
+     * Click2Crash.Just used for test.
+     */
+    private void click2Crash() {
+        if (mNullObject.id > 0) { // here will crash
+            Toast.makeText(MainActivity.this, "Click not crash!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(MainActivity.this, "Click not crash!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
